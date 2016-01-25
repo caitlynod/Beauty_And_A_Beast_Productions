380f6ddf101e31e5bbb3f9fba64f2fcc3617544c
caitlynod <caitlynodriscoll@gmail.com>
2016-01-25 16:08:15 -0500

    Controller Scripts

---
+++ followPlayer.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+private var target : GameObject;
+function Start () {
+	target = GameObject.Find("Player");
+}
+
+function Update () {
+	transform.position.x = target.transform.position.x+1;
+	transform.position.y = target.transform.position.y+.5;
+}
