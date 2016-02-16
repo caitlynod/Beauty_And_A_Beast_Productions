4f762a0e48aab110fb07a9ddd5879059f1bb60a0
led-x <day_jason@hotmail.com>
2016-02-16 03:11:43 -0500

    Added Rotate Script

---
+++ rotate.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+/*
+
+public var rotationAmount : float = 5.0;
+
+
+function Start () {
+
+}
+
+function Update () {
+
+transform.Rotate(rotationAmount,0,0);
+
+}
+*/
+
+public var rotate : float = 5.0;
+
+function Update() {
+	var rotateDelta : float = rotate*Time.deltaTime;
+	//move the object 10 units per second vs 10 units per frame
+	
+	transform.Rotate(rotateDelta, 0, 0);
+}
+
+
+
+
