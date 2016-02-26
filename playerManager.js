e52201bf9d69e6ad644db532a5847b2e3b31d434
caitlynod <caitlynodriscoll@gmail.com>
2016-02-25 21:27:05 -0500

    Updated health values

--- playerManager.js
+++ playerManager.js
@@ -1,6 +1,6 @@
 ﻿#pragma strict
 import System.Collections.Generic;
-var health : int = 100;
+var health : int = 6;
 public var fruit:int = 0;
 public var utensil:int = 0;
 public var score: int = 0;
@@ -9,24 +9,24 @@ var inventory = new Dictionary.<String,int>();
 inventory["power"] = 0;
 
 function OnTriggerEnter(other:Collider) {
-	if(other.tag =="fruit" && fruit < 100) {
+	if(other.tag =="fruit") {
 		fruit++;
 		score++;
 		Destroy(other.gameObject);
 	}
-	if(other.tag =="utensil" && fruit < 100) {
+	if(other.tag =="utensil") {
 		utensil++;
 		score++;
 		Destroy(other.gameObject);
 	}
 
 
-	if(other.tag =="health" && health < 100) {
+	if(other.tag =="health" && health < 6) {
 	health++;
 	Destroy(other.gameObject);
 	}
 	
-	if(other.tag=="health" && health==100){
+	if(other.tag=="health" && health==6){
 	Destroy(other.gameObject);
 	}
 
