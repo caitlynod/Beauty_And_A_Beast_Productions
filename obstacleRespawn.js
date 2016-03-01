a64c6b6134551011e9b5025b1b4e536168385ccd
Tatiana <tanyauspenskaya@gmail.com>
2016-02-29 23:37:15 -0500

    Added files via upload

---
+++ obstacleRespawn.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var spawn_point : Transform;
+var GameCamera : Transform;
+
+function playerRespawn() {
+	transform.position = spawn_point.position;
+}
+
+function Update () {
+	GameCamera.LookAt(this.transform);
+	var playerPos = transform.position;
+
+	if (playerPos.y <= -8) {
+		playerRespawn();
+	}
+}
