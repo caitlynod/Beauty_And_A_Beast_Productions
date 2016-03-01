a229dac222693d84fc39ad25c16f9c963834daed
Tatiana <tanyauspenskaya@gmail.com>
2016-02-29 23:36:09 -0500

    Added files via upload

---
+++ dogAnimations.js
@@ -0,0 +1,26 @@
+﻿var anim: Animation;
+var target;
+
+function Start() {
+	anim = GetComponent.<Animation>();
+	target = GameObject.Find("Player").GetComponent(playerController);
+}
+
+// Make the character fade between an idle and a run animation 
+// when the player starts to move.
+function Update () {
+	if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1){
+	anim.CrossFade("Walk Dog");
+	if(Input.GetButton("Fire1") && Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1){
+	anim.CrossFade("Run Dog");
+	}
+	}else if(Input.GetButton("Jump")){
+	anim.CrossFade("Fall Dog");
+	}else{
+	anim.CrossFade("Idle 2 Dog");
+	}
+	if(!target.isGrounded()){
+	anim.CrossFade("Fall Dog");
+	}
+		
+}
