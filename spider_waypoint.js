efa92f292c72665eb26349de65ff6a3b71e2f0f6
led-x <day_jason@hotmail.com>
2016-03-15 06:47:32 -0400

    Added wolf and Spider way point scripts

---
+++ spider_waypoint.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+public var moveSpeed : float;
+var moveDirection : String = "Forward";
+// public var rotate : float = 1;
+
+function Update () {
+
+	if(moveDirection == "Forward") {
+		transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+	} else {
+		transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
+	}
+
+}
+
+function OnTriggerEnter(hit : Collider) {
+	if((hit.gameObject.tag == "waypoint") && (moveDirection == "Forward"))
+	{
+		transform.Rotate(0, 180, 0);
+		moveDirection = "Forward"; 
+		
+	} else
+	{
+		transform.Rotate(0, 180, 0);
+		moveDirection = "Backward";
+	}
+}
---
+++ wolf_waypoint.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+public var moveSpeed : float;
+var moveDirection : String = "Left";
+// public var rotate : float = 1;
+
+function Update () {
+
+	if(moveDirection == "Left") {
+		transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
+	} else {
+		transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+	}
+
+}
+
+function OnTriggerEnter(hit : Collider) {
+	if((hit.gameObject.tag == "waypoint") && (moveDirection == "Right"))
+	{
+		transform.Rotate(0, 180, 0);
+		moveDirection = "Right"; 
+		
+	} else
+	{
+		transform.Rotate(0, 180, 0);
+		moveDirection = "Left";
+	}
+}
