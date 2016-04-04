07de335d0c7c86eae603c9961e9622f6b9cc9937
led-x <day_jason@hotmail.com>
2016-04-04 14:48:49 -0400

    enemy script
    Script so the wolves and spider will die when attacked and drop a heart

---
+++ Enemy.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var Health = 1;
+var anim: Animation;
+var HeartRed : Transform;
+
+function Update()
+{
+	if(Health <= 0)
+	{
+		Dead();
+	}
+}
+
+function ApplyDamage (Damage : int)
+{
+	Health -= Damage; 
+}
+
+function Dead()
+{
+	Destroy (gameObject);
+	Instantiate(HeartRed,transform.position, Quaternion.identity);
+	
+}
