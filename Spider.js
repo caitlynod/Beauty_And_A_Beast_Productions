d298868970a626c47a00a9e025e3d1b3cec7ee07
led-x <day_jason@hotmail.com>
2016-04-11 00:07:57 -0400

    Spider drop scirpt

---
+++ Spider.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var Health = 1;
+var Orange : Transform;
+
+function Update()
+{
+	if(Health <= 0)
+	{
+		Dead();
+	}
+}
+
+function ApplyDamage (Damage : int)
+{
+	Health -= Damage; 
+}
+
+function Dead()
+{
+	Destroy (gameObject);
+	Instantiate(orange,transform.position, Quaternion.identity);
+	Instantiate(orange,transform.position, Quaternion.identity);
+	Instantiate(orange,transform.position, Quaternion.identity);
+	
+}
